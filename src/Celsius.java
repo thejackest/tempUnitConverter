45c180d5c274a7a5b2d9ca51a219b5f39e4a0bb0
Jackest Gong <jackestgong@gmail.com>
2019-08-15 06:47:40 +1200

    override method in subclass Celsius, test passed

--- src/Celsius.java
+++ src/Celsius.java
@@ -8,4 +8,9 @@ public class Celsius extends tempUnitConverter
     public float toKelvin(){
         return this.getValue() + 273;
     }
+    
+    @Override
+    public float toFahrenheit(){
+        return (float)9/5 * this.getValue() + 32;
+    }
 }
