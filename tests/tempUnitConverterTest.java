13624178b2f83e7b321e63684fd962e9e4103d85
Jackest Gong <jackestgong@gmail.com>
2019-08-15 06:09:09 +1200

    TEST: convert kelvin to celsius, method not implemented

--- tests/tempUnitConverterTest.java
+++ tests/tempUnitConverterTest.java
@@ -14,4 +14,15 @@ class tempUnitConverterTest {
         Kelvin unit = new Kelvin(30);
         assertEquals(30,unit.getValue());
     }
+
+    @Test
+    void convertFromKelvinToCelsius(){
+        //given 20 degrees Kelvin
+        Kelvin input =  new Kelvin(20);
+        //convert it to Celsius
+        Celsius actual = new Celsius(input.toCelsius());
+        //the result should be -253
+        float expected = -253;
+        assertEquals(expected,actual.getValue(),"correct");
+    }
 }
