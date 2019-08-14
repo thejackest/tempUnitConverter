af52140a79558f12063495899f69a3141506e0cd
Jackest Gong <jackestgong@gmail.com>
2019-08-15 06:15:38 +1200

    override method in subclass Kelvin, test passed

--- src/Kelvin.java
+++ src/Kelvin.java
@@ -2,4 +2,9 @@ public class Kelvin extends tempUnitConverter {
     public Kelvin(float value) {
         super(value);
     }
+
+    @Override
+    public float toCelsius(){
+        return this.getValue() -273;
+    }
 }
